c8519c2beefd590e7d64afbdde162b271cb5ccbd
Oleh-Shaklein <145619938+Oleh-Shaklein@users.noreply.github.com>
2023-09-25 03:31:38 +0300

    Lab_01
    Додано код першої лабораторної роботи

---
+++ src/Lab_01_KZP.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Lab_01_KZP {	 
+
+	public static void main(String[] args) throws FileNotFoundException{
+		int nRows;
+		char[][] arr;
+		String filler;
+
+		Scanner in = new Scanner(System.in);
+		File dataFile = new File("MyFile.txt");
+		
+		PrintWriter fout = new PrintWriter(dataFile);
+
+		System.out.print("Введіть розмір квадратної матриці: ");
+		
+		nRows = in.nextInt();
+		in.nextLine();
+
+		System.out.print("\nВведіть символ-заповнювач: ");
+
+		filler = in.nextLine();
+		
+		
+		System.out.println("\nРезультат: ");
+		
+		
+		exit:
+			if(filler.length()==1) {
+				for(int i = (nRows/2)+(nRows%2-1); i>= (1+(nRows%2-1)); i--) {
+					for(int j=0; j<=(nRows-i); j++) {
+						System.out.print("  ");
+						fout.print("  ");
+					}
+	            
+					for(int k=0; k<=(2*i)-(nRows%2-1); k++) {
+						System.out.print(filler+" ");
+						fout.print(filler+" ");
+					}
+					System.out.println();
+				}
+
+				for(int i = 0; i<=(nRows/2)+(nRows%2-1); i++) {
+					for(int j=0; j<=(nRows-i); j++) {
+						System.out.print("  ");
+						fout.print("  ");
+					}
+					for(int k=0; k<=(2*i)-(nRows%2-1); k++) {
+						System.out.print(filler+" ");
+						fout.print(filler+" ");
+					}
+					System.out.println();
+				}
+			} else if (filler.length() == 0){
+				System.out.print("\nНе введено символ заповнювач");
+				break exit;
+			}
+			else {
+				System.out.print("\nЗабагато символів заповнювачів");
+				break exit;
+			}
+			System.out.print("\n");
+			fout.print("\n");
+			fout.flush();
+			fout.close();
+	}	
+}
