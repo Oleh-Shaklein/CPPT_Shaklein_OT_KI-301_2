dcf60b674db74bce96e1947f95cf6c38b32de06f
Oleh-Shaklein <145619938+Oleh-Shaklein@users.noreply.github.com>
2023-09-26 05:59:19 +0300

    Add files via upload
    Перероблено лабораторну роботу номер 1 згідно вимог

--- src/Lab_01_KZP.java
+++ src/Lab_01_KZP.java
@@ -1,59 +1,97 @@
 import java.io.*;
 import java.util.*;
-import java.util.Scanner;
 
 public class Lab_01_KZP {	 
 
 	public static void main(String[] args) throws FileNotFoundException{
 		int nRows;
-		char[][] arr;
 		String filler;
-
+		//String zero = "0";
 		Scanner in = new Scanner(System.in);
-		File dataFile = new File("MyFile.txt");
-		
-		PrintWriter fout = new PrintWriter(dataFile);
 
 		System.out.print("Введіть розмір квадратної матриці: ");
 		
 		nRows = in.nextInt();
 		in.nextLine();
-
+		
 		System.out.print("\nВведіть символ-заповнювач: ");
 
 		filler = in.nextLine();
 		
+		File dataFile = new File("MyFile.txt");
+		PrintWriter fout = new PrintWriter(dataFile);
 		
 		System.out.println("\nРезультат: ");
 		
-		
+		String [][] arr0 = new String [nRows][nRows];
 		exit:
 			if(filler.length()==1) {
-				for(int i = (nRows/2)+(nRows%2-1); i>= (1+(nRows%2-1)); i--) {
-					for(int j=0; j<=(nRows-i); j++) {
-						System.out.print("  ");
-						fout.print("  ");
-					}
-	            
-					for(int k=0; k<=(2*i)-(nRows%2-1); k++) {
-						System.out.print(filler+" ");
-						fout.print(filler+" ");
-					}
-					System.out.println();
+				for(int i = nRows-1; i >= 0; i--) {
+				      for(int j = 0; j < nRows; j++) {
+				        if ((i > j || i+1 > nRows - j)&&(i < j || i+1 < nRows - j)) {
+				        	//i > j
+				        	
+//				        	0 0 0 0 0 0 0 0 0 1 
+//				        	0 0 0 0 0 0 0 0 1 1 
+//				        	0 0 0 0 0 0 0 1 1 1 
+//				        	0 0 0 0 0 0 1 1 1 1 
+//				        	0 0 0 0 0 1 1 1 1 1 
+//				        	0 0 0 0 1 1 1 1 1 1 
+//				        	0 0 0 1 1 1 1 1 1 1 
+//				        	0 0 1 1 1 1 1 1 1 1 
+//				        	0 1 1 1 1 1 1 1 1 1 
+//				        	1 1 1 1 1 1 1 1 1 1 
+				        	
+				        	//i+1 > nRows - j
+				        	
+//				        	1 0 0 0 0 0 0 0 0 0 
+//				        	1 1 0 0 0 0 0 0 0 0 
+//				        	1 1 1 0 0 0 0 0 0 0 
+//				        	1 1 1 1 0 0 0 0 0 0 
+//				        	1 1 1 1 1 0 0 0 0 0 
+//				        	1 1 1 1 1 1 0 0 0 0 
+//				        	1 1 1 1 1 1 1 0 0 0 
+//				        	1 1 1 1 1 1 1 1 0 0 
+//				        	1 1 1 1 1 1 1 1 1 0 
+//				        	1 1 1 1 1 1 1 1 1 1 
+				        	
+				        	//i < j
+				        	
+//				        	1 1 1 1 1 1 1 1 1 1 
+//				        	1 1 1 1 1 1 1 1 1 0 
+//				        	1 1 1 1 1 1 1 1 0 0 
+//				        	1 1 1 1 1 1 1 0 0 0 
+//				        	1 1 1 1 1 1 0 0 0 0 
+//				        	1 1 1 1 1 0 0 0 0 0 
+//				        	1 1 1 1 0 0 0 0 0 0 
+//				        	1 1 1 0 0 0 0 0 0 0 
+//				        	1 1 0 0 0 0 0 0 0 0 
+//				        	1 0 0 0 0 0 0 0 0 0 
+				        	
+				        	//i+1 < nRows - j
+				        	
+//				        	1 1 1 1 1 1 1 1 1 1 
+//				        	0 1 1 1 1 1 1 1 1 1 
+//				        	0 0 1 1 1 1 1 1 1 1 
+//				        	0 0 0 1 1 1 1 1 1 1 
+//				        	0 0 0 0 1 1 1 1 1 1 
+//				        	0 0 0 0 0 1 1 1 1 1 
+//				        	0 0 0 0 0 0 1 1 1 1 
+//				        	0 0 0 0 0 0 0 1 1 1 
+//				        	0 0 0 0 0 0 0 0 1 1 
+//				        	0 0 0 0 0 0 0 0 0 1 
+				        	
+				          arr0[i][j] = " ";
+				        } else {
+				          arr0[i][j] = filler;
+				        }
+				        System.out.print(arr0[i][j] + " ");
+				        fout.write(arr0[i][j] + " ");
+				      }
+				      System.out.println();
+				      fout.println();
 				}
-
-				for(int i = 0; i<=(nRows/2)+(nRows%2-1); i++) {
-					for(int j=0; j<=(nRows-i); j++) {
-						System.out.print("  ");
-						fout.print("  ");
-					}
-					for(int k=0; k<=(2*i)-(nRows%2-1); k++) {
-						System.out.print(filler+" ");
-						fout.print(filler+" ");
-					}
-					System.out.println();
-				}
-			} else if (filler.length() == 0){
+			} else if (filler.length() == 0){	
 				System.out.print("\nНе введено символ заповнювач");
 				break exit;
 			}
